8e8a139b2eb666399b0280c9427c241834ba0abf
Hamza Khan <hamzakhan44490>
2026-09-08 17:01:47 +0500

    Update requirements

---
+++ student_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Student(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    subject = models.CharField(max_length=100)
+    marks = models.IntegerField()
+
