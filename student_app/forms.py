c5b418a0b1b149f7033eb50deca05d6a7a2423cc
Hamza Khan <hamzakhan44490>
2026-09-22 19:06:44 +0500

    phase 4

--- student_app/forms.py
+++ student_app/forms.py
@@ -5,7 +5,7 @@ from .models import Student
 class StudentRegistrationForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ["name",  "marks", "email", "enrollment_date", "courses"]
+        fields = ["name", "marks", "email", "enrollment_date", "courses"]
         widgets = {
             "name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Name"}),
             "marks": forms.TextInput(attrs={"class": "form-control", "placeholder": "Marks"}),
